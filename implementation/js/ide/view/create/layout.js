6ae6d40ef2885ffedaf25ff84097c77b7f210ece
mr-beaver <beavercoding@gmail.com>
2017-01-31 20:11:32 -0800

    first draft of dragging mechanism;

--- implementation/js/ide/view/create/layout.js
+++ implementation/js/ide/view/create/layout.js
@@ -108,88 +108,275 @@
 			node.setAttribute('point-id', id);
 
 			$node.one('click', function(e){
-				clickCallback(e, $node);
+				that.clickCallback(e, $node);
 			});
 
 			$node.one('mousedown', function(e){
-				mousedownCallback(e, $node, $body, that);
+				that.mousedownCallback(e, $node, $body);
 			});
+		},
+		clickCallback: function(e, $node){
+			var that = this;
+			//prevent default
+			e.preventDefault();
+			//stop event poping to parent element
+			e.stopPropagation();
+			
+			//trigger end point click event
+			app.coop('click-endpoint', e);
+
+			$node.one('click', function(event){
+				that.clickCallback(event, $node);
+			});
+		},
+		mousedownCallback: function(e, $node, $body){
+			var that = this,
+				id = $node.attr('point-id');
+			//prevent default
+			e.preventDefault();
+			//stop event poping to parent element
+			e.stopPropagation();
+			//set dragging flag true
+			this.dragging = true;
+
+			var constrains = this.checkMovingConstrain(id),
+				originalCoords = {
+					x: app._global.endPoints[id].x,
+					y: app._global.endPoints[id].y
+				};
+
+			//use body as the mousemove view port
+			$body
+			.on('mousemove', function(e){
+				//prevent default
+				e.preventDefault();
+
+				//unbind original click event and mousedown event,
+				//unbind click until mousemove to avoid mousedown and click event conflict
+				$node.unbind('click').unbind('mousedown');
+
+				//check constrains
+				var hPer = e.pageX / that.$el.width() * 100,
+					vPer = e.pageY / that.$el.height() * 100;
+
+				if(hPer > constrains.limits.xmin && hPer < constrains.limits.xmax && vPer > constrains.limits.ymin && vPer < constrains.limits.ymax){
+					//update all related lines and end points
+					that.updateRelated(constrains.hlines, constrains.vlines, constrains.endPoints, e, $node.attr('point-id'), originalCoords);
+				}
+				// //update lines and points positions only when it meets the constrain
+				// if(checkMovingConstrain($node.attr('point-id'), View)){
+
+				// 	//!!TBD: update all lines and points
+				// 	$node.attr('cx', e.pageX);
+
+				// }else{
+				// 	_.debounce(function(){
+				// 		app.notify('Cannot be moved this far.', 'This line attached to the current end point cannot be moved this far!', 'error', {icon: 'fa fa-reddit-alien'});
+				// 	}, 500);
+				// }
+			})
+			.one('mouseup', function(e){
+				//prevent default
+				e.preventDefault();
+				//unbind mousemove event on body
+				$body.unbind('mousemove');
+				//reset dragging flag to false
+				that.dragging = false;
+				//mouseup has a bit delay. use defer to register click and mousedown event on node again
+				_.defer(function(){
+					//click
+					$node.one('click', function(event){
+						that.clickCallback(event, $node);
+					});
+					//mousedown
+					$node.one('mousedown', function(event){
+						that.mousedownCallback(event, $node, $body);
+					});
+				});
+			});
+		},
+		checkMovingConstrain: function(id/*point id*/){
+			//fetch the point
+			var point = app._global.endPoints[id],
+				directions = ['left', 'right', 'top', 'bottom'],
+				vem = (parseFloat(getComputedStyle(document.body).fontSize)) / this.$el.height() * 100,
+				hem = (parseFloat(getComputedStyle(document.body).fontSize)) / this.$el.width() * 100,
+				temp;
+
+			var limits = {
+				xmin : 2 * hem, xmax: 100 - 2 * hem, ymin: 2 * vem, ymax: 100 - 2 * vem
+			};//store all the limits
+
+			var extensions = {
+				left: 0, right: 0, top: 0, bottom: 0,
+				hlines: [],//store all the horizontal lines controlled by this point
+				vlines: [],//store all the vertical lines controlled by this point
+				endPoints: []//store all the end points controlled by this point
+			};
+			
+			//check the extension limit of each direction
+			_.each(directions, function(direction){
+				var orientation = (direction === 'left' || direction === 'right') ? 'h' : 'v', tempLine;
+
+				temp = point;
+				if(!_.contains(extensions.endPoints, temp.id))
+					extensions.endPoints.push(temp.id);//store the point
+
+				//trace down the last point in this direction
+				while(temp[direction]){
+					//fetch next line
+					tempLine = _.find(app._global[(orientation === 'h') ? 'horizontal-line' : 'vertical-line'], function(line){ return line.id === temp[direction] });
+					//store line walked through
+					(orientation === 'h') ? extensions.hlines.push(tempLine.id) : extensions.vlines.push(tempLine.id);
+					//get next point
+					temp = app._global.endPoints[tempLine[direction]];
+					//store next point
+					extensions.endPoints.push(temp.id);
+				}
+
+				switch(direction){
+					case 'left':
+						extensions.left = temp.x;
+					break;
+					case 'right':
+						extensions.right = temp.x;
+					break;
+					case 'top':
+						extensions.top = temp.y;
+					break;
+					case 'bottom':
+						extensions.bottom = temp.y;
+					break;
+				}
+			});
+
+			//now we have extension limits, check with horizontal and vertical collections to setup the real limit
+			//horizontal
+			_.each(app._global['horizontal-line'], function(hline){
+				if(
+					!_.contains(extensions.hlines, hline.id) &&
+					(
+						(hline.x1 < extensions.right /** (1 + app._global.tolerance)*/ && hline.x1 > extensions.left/* * (1 - app._global.tolerance)*/) ||
+						(hline.x2 < extensions.right /** (1 + app._global.tolerance)*/ && hline.x2 > extensions.left/* * (1 - app._global.tolerance)*/)
+					)
+				){
+					if(hline.y <= point.y * (1 + app._global.tolerance)){//ymin
+						temp = hline.y + 2 * vem;
+						if(temp > limits.ymin)
+							limits.ymin = temp;
+					}
+
+					else if(hline.y >= point.y * (1 - app._global.tolerance)){//ymax
+						temp = hline.y - 2 * vem;
+						if(temp < limits.ymax)
+							limits.ymax = temp;
+					}
+				}
+			});
+
+			//vertical
+			_.each(app._global['vertical-line'], function(vline){
+				if(	
+					!_.contains(extensions.vlines, vline.id) &&
+					((vline.y1 < extensions.bottom/* * (1 + app._global.tolerance)*/ && vline.y1 > extensions.top/* * (1 - app._global.tolerance)*/) ||
+					(vline.y2 < extensions.bottom/* * (1 + app._global.tolerance)*/ && vline.y2 > extensions.top/* * (1 - app._global.tolerance)*/))
+
+				){
+					if(vline.x <= point.x * (1 + app._global.tolerance)){//xmin
+						temp = vline.x + 2 * hem;
+						if(temp > limits.xmin)
+							limits.xmin = temp;
+					}
+
+					else if(vline.x >= point.x * (1 - app._global.tolerance)){//xmax
+						temp = vline.x - 2 * hem;
+						if(temp < limits.xmax)
+							limits.xmax = temp;
+					}
+				}
+			});
+
+			return {
+				limits: limits,
+				hlines: extensions.hlines,
+				vlines: extensions.vlines,
+				endPoints: extensions.endPoints
+			};
+		},
+		updateRelated: function(hlines, vlines, points, event, anchorId, originalCoords){
+			var magneted = false;
+
+			var height = this.$el.height(),
+				width = this.$el.width();
+
+			//update horizontal lines
+			_.each(hlines, function(id){
+				var line = _.find(app._global['horizontal-line'], function(hline){ return hline.id === id; }),
+					yCoord = trimNumber(event.pageY / height * 100);
+				//update line itself
+				line.y = yCoord;
+				//update its end points
+				//left
+				app._global.endPoints[line.left].y = yCoord;
+				//right
+				app._global.endPoints[line.right].y = yCoord;
+			});
+
+			//update vertical lines
+			_.each(vlines, function(id){
+				var line = _.find(app._global['vertical-line'], function(vline){ return vline.id === id; }),
+					xCoord = trimNumber(event.pageX / width * 100);
+				//update line tiself
+				line.x = xCoord;
+				//update its end points
+				//top
+				app._global.endPoints[line.top].x = xCoord;
+				//bottom
+				app._global.endPoints[line.bottom].x = xCoord;
+			});
+
+			//update length of lines that connected to the related points
+			_.each(points, function(pointId){
+				var point = app._global.endPoints[pointId],
+					line;
+				//top line
+				if(point.top){
+					line = _.find(app._global['vertical-line'], function(vline){ return vline.id === point.top; });
+					line.y2 = point.y;
+				}
+
+				//bottom line
+				if(point.bottom){
+					line = _.find(app._global['vertical-line'], function(vline){ return vline.id === point.bottom; });
+					line.y1 = point.y;
+				}
+
+				//left line
+				if(point.left){
+					line = _.find(app._global['horizontal-line'], function(hline){ return hline.id === point.left; });
+					line.x2 = point.x;
+				}
+
+				if(point.right){
+					line = _.find(app._global['horizontal-line'], function(hline){ return hline.id === point.right; });
+					line.x1 = point.x;
+				}
+
+			});
+
+			//check whether anchor can be merged with other points
+				
+
+			this.redrawAll();
+
+			return magneted;
 		},
 	});
 
-	function clickCallback(e, $node){
-		//prevent default
-		e.preventDefault();
-		//stop event poping to parent element
-		e.stopPropagation();
-		
-		//trigger end point click event
-		app.coop('click-endpoint', e);
-
-		$node.one('click', function(event){
-			clickCallback(event, $node);
-		});
+	//trim number only leave two digits after decimal point
+	function trimNumber(number){
+		return parseFloat(number.toFixed(2));
 	}
-
-	function mousedownCallback(e, $node, $body, View){
-		//prevent default
-		e.preventDefault();
-		//stop event poping to parent element
-		e.stopPropagation();
-		//set dragging flag true
-		View.dragging = true;
-
-		//use body as the mousemove view port
-		$body
-		.on('mousemove', function(e){
-			//prevent default
-			e.preventDefault();
-
-			//unbind original click event and mousedown event,
-			//unbind click until mousemove to avoid mousedown and click event conflict
-			$node.unbind('click').unbind('mousedown');
-
-			//update lines and points positions only when it meets the constrain
-			if(checkMovingConstrain($node.attr('point-id'))){
-
-				//!!TBD: update all lines and points
-				$node.attr('cx', e.pageX);
-
-			}/*else{
-				_.debounce(function(){
-					app.notify('Cannot be moved this far.', 'This line attached to the current end point cannot be moved this far!', 'error', {icon: 'fa fa-reddit-alien'});
-				}, 500);
-			}*/
-		})
-		.one('mouseup', function(e){
-			//prevent default
-			e.preventDefault();
-			//unbind mousemove event on body
-			$body.unbind('mousemove');
-			//reset dragging flag to false
-			View.dragging = false;
-			//mouseup has a bit delay. use defer to register click and mousedown event on node again
-			_.defer(function(){
-				//click
-				$node.one('click', function(event){
-					clickCallback(event, $node);
-				});
-				//mousedown
-				$node.one('mousedown', function(event){
-					mousedownCallback(event, $node, $body, View);
-				});
-			});
-		});
-	}
-
-	//TBD
-	function checkMovingConstrain(id/*point id*/){
-
-
-		//!!check xmax, xmin, ymax, ymin at least 2em!!
-
-
-		return false;
-	}
+	
 
 })(Application);
